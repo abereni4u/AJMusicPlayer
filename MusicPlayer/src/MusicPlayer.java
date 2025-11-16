b4cc64b3e3a1407b61a9b1303d94daac892ba63b
Abereni4u <abereni4u@gmail.com>
2025-11-16 00:52:38 -0500

    Add music file identification and retrieval procedure

--- MusicPlayer/src/MusicPlayer.java
+++ MusicPlayer/src/MusicPlayer.java
@@ -1,15 +1,19 @@
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.*;
 import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class MusicPlayer {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         Scanner directoryScanner = new Scanner(System.in);
         System.out.println("Please enter a directory: ");
         String userDirectory = directoryScanner.nextLine();
+
+        ArrayList<Path> userMusic = new ArrayList<>();
 
         // Convert user's string into a path and test whether it's a valid directory.
 
@@ -20,7 +24,7 @@ public class MusicPlayer {
            System.out.println("Please enter a valid directory.");
            System.out.println("Do you wish to continue? (Y/N) ");
            continueOrNot = directoryScanner.nextLine();
-           if(continueOrNot.toLowerCase().equals("n"))
+           if(continueOrNot.equalsIgnoreCase("n"))
                break;
            System.out.println("Please enter a directory: ");
            userDirectory = directoryScanner.nextLine();
@@ -29,6 +33,21 @@ public class MusicPlayer {
 
         if(continueOrNot.equals("y") || (continueOrNot.equals(""))){
             System.out.println("You entered, " + userDirectory);
+        }
+
+        // At this point we've confirmed the user's directory is valid
+        Path userDirectoryPath = Paths.get(userDirectory);
+        try (DirectoryStream<Path> userDirectoryEntries = Files.newDirectoryStream(userDirectoryPath)) {
+            for (Path userDirectoryEntry : userDirectoryEntries) {
+                String fileType = Files.probeContentType(userDirectoryEntry);
+                if(fileType != null && fileType.split("/")[0].equals("audio")){
+                    userMusic.add(userDirectoryEntry);
+                }
+            }
+        }
+
+        for(Path aPath : userMusic){
+            System.out.println(aPath.getFileName());
         }
 
         System.out.println("Program completed");
