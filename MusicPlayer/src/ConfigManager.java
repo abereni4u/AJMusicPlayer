dddb14d284ba5f2233d782e76b816fe8ae87b251
Abereni4u <abereni4u@gmail.com>
2025-11-30 00:43:29 -0500

    Add structure comments to ConfigManager class

--- MusicPlayer/src/ConfigManager.java
+++ MusicPlayer/src/ConfigManager.java
@@ -1,4 +1,35 @@
 public class ConfigManager {
-    
+
+    // Return a MusicLibrary object
+
+        // Check if config folder exists
+
+            // If it exists:
+
+                    // Read directories from config file
+
+                    // Deserialize MusicLibrary
+                        // Update MusicLibrary
+                            // Check for unlinks and deletions
+
+                    // Serialize MusicLibrary
+
+                    // Return updated MusicLibrary object
+
+            // If it doesn't exist:
+
+                    // Get a valid directory from the user
+
+                    // Scan directory for music files
+
+                    // Create config folder and config file
+
+                    // Add directory to config file
+
+                    // Create MusicLibrary from files
+
+                    // Serialize MusicLibrary
+
+                    // Return updated MusicLibrary
 
 }
