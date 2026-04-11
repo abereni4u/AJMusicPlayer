31e09628929d33337742d5858e2d12aeb894b21f
abereni4u <aberenijopuiyo@gmail.com>
2026-04-11 13:18:17 -0400

    Add createTrack public constructor for Track class

--- MusicPlayer/src/main/java/Track.java
+++ MusicPlayer/src/main/java/Track.java
@@ -1,3 +1,14 @@
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.AudioHeader;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.TagException;
+
+import java.io.IOException;
 import java.io.Serializable;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,12 +21,36 @@ public class Track implements Serializable {
     private transient Path filePath;
     private final String title;
 
+    private final String artist;
+
+    private final String album;
+
+    private final int trackLength;
+
     private String pathString = "";
 
-    public Track(String pathString) {
-        this.pathString = pathString;
-        this.filePath = Paths.get(pathString);
-        this.title = String.valueOf(filePath.getFileName());
+    public static Track createTrack(String pathString) throws CannotReadException, TagException, InvalidAudioFrameException, ReadOnlyFileException, IOException {
+
+        Path trackFilePath = Paths.get(pathString);
+
+
+        AudioFile f = AudioFileIO.read(trackFilePath.toFile());
+        Tag tag = f.getTag();
+        AudioHeader permTags = f.getAudioHeader(); // Tags that can't be changed here
+
+        int tagTrackLength = permTags.getTrackLength();
+        String tagTitle = tag.getFirst(FieldKey.TITLE);
+        String tagArtist = tag.getFirst(FieldKey.ARTIST);
+        String tagAlbum = tag.getFirst(FieldKey.ALBUM);
+
+        return new Track(tagTrackLength, tagTitle, tagArtist, tagAlbum);
+    }
+
+    private Track(int trackLength, String title, String artist, String album){
+        this.trackLength = trackLength;
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
     }
 
     public Path getFilePath(){
